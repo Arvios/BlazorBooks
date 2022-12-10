58f6aabebf9b5aa67c44295b9b556ad928600797
Ionian Blade <vipmen5555@gmail.com>
2022-12-10 21:49:40 +0300

    Добавьте файлы проекта.

---
+++ BlazorBooks/Server/Data/DataContext.cs
@@ -0,0 +1,39 @@
+﻿namespace BlazorBooks.Server.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) 
+		{
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Title = "Test",
+					Description = "Test",
+					ImageUrl = "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg",
+					Price = 1,
+				},
+				new Product
+				{
+					Id = 2,
+					Title = "Test",
+					Description = "Test",
+					ImageUrl = "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg",
+					Price = 1,
+				},
+				new Product
+				{
+					Id = 3,
+					Title = "Test",
+					Description = "Test",
+					ImageUrl = "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg",
+					Price = 1,
+				});
+		}
+		public DbSet<Product> Products { get; set; }
+	}
+}
---
+++ BlazorBooks/Server/Migrations/20221210183355_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorBooks.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Test", "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg", 1m, "Test" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Test", "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg", 1m, "Test" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Test", "https://upload.wikimedia.org/wikipedia/ru/6/65/Tokyo_Ghoul_volume_1.jpg", 1m, "Test" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
